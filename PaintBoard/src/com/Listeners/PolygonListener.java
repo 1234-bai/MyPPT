3a9fec2c1674a4ee362903df08fa9d974730d399
QianXmY <863119860@qq.com>
2021-11-07 16:49:40 +0800

    多边形监听器

---
+++ PaintBoard/src/com/Listeners/PolygonListener.java
@@ -0,0 +1,69 @@
+package com.Listeners;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Line2D;
+
+public class PolygonListener extends DrawListener {
+    int[] x = new int[20];  //x坐标集合
+    int[] y = new int[20];  //y坐标集合
+    int n = 0;    //点的个数
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        //记录点坐标
+        x[n] = e.getX();
+        y[n] = e.getY();
+        n++;
+
+        //测试用，控制台显示鼠标点击的坐标
+        System.out.println(x[n - 1] + ", " + y[n - 1]);
+
+        //当前点与第一个点的距离在8以内，则形成收尾相连的多边形
+        if ((x[n - 1] - x[0]) * (x[n - 1] - x[0]) + (y[n - 1] - y[0]) * (y[n - 1] - y[0]) <= 64 && n > 2) {
+            x[n - 1] = x[0];
+            y[n - 1] = y[0];
+            Polygon polygon = new Polygon(x, y, n);
+            getListenerPen().drawImage(getDrawBoard().getDrawBoardCopy(), 0, 0, null);  //最后一次选点多画的线删掉
+            super.getListenerPen().draw(polygon);
+            super.getListenerPenCopy().draw(polygon);
+            n = 0;
+        }
+
+        //将点连线
+        if(n>1){
+            Line2D line = new Line2D.Double(x[n - 2],y[n - 2],x[n - 1],y[n - 1]);
+            getListenerPen().draw(line);
+            getListenerPenCopy().draw(line);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+        //当多边形未完成时线条随鼠标移动
+        if (n != 0) {
+            getListenerPen().drawImage(getDrawBoard().getDrawBoardCopy(), 0, 0, null);
+            Line2D line = new Line2D.Double(x[n - 1], y[n - 1], e.getX(), e.getY());
+            getListenerPen().draw(line);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
