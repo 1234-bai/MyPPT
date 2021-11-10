e4a7513632450fa9b08b1c623148669a5dd3c2bf
QianXmY <863119860@qq.com>
2021-11-10 23:50:50 +0800

    对副本的名字做了一点点修改。对副本载入函数（refresh）做出了修改。

--- PaintBoard/src/com/Listeners/PolygonListener.java
+++ PaintBoard/src/com/Listeners/PolygonListener.java
@@ -6,6 +6,9 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Line2D;
 
 public class PolygonListener extends DrawListener {
+
+    private static final int lastDict = 10;
+
     int[] x = new int[20];  //x坐标集合
     int[] y = new int[20];  //y坐标集合
     int n = 0;    //点的个数
@@ -13,7 +16,6 @@ public class PolygonListener extends DrawListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
         //记录点坐标
         x[n] = e.getX();
         y[n] = e.getY();
@@ -23,13 +25,13 @@ public class PolygonListener extends DrawListener {
         System.out.println(x[n - 1] + ", " + y[n - 1]);
 
         //当前点与第一个点的距离在8以内，则形成收尾相连的多边形
-        if ((x[n - 1] - x[0]) * (x[n - 1] - x[0]) + (y[n - 1] - y[0]) * (y[n - 1] - y[0]) <= 64 && n > 2) {
+        if ((x[n - 1] - x[0]) * (x[n - 1] - x[0]) + (y[n - 1] - y[0]) * (y[n - 1] - y[0]) <= lastDict*lastDict  && n > 2) {
             x[n - 1] = x[0];
             y[n - 1] = y[0];
             Polygon polygon = new Polygon(x, y, n);
-            getListenerPen().drawImage(getDrawBoard().getDrawBoardCopy(), 0, 0, null);  //最后一次选点多画的线删掉
+            getDrawBoard().refresh();  //最后一次选点多画的线删掉
             super.getListenerPen().draw(polygon);
-            super.getListenerPenCopy().draw(polygon);
+            super.getListenerPen_copy().draw(polygon);
             n = 0;
         }
 
@@ -37,26 +39,15 @@ public class PolygonListener extends DrawListener {
         if(n>1){
             Line2D line = new Line2D.Double(x[n - 2],y[n - 2],x[n - 1],y[n - 1]);
             getListenerPen().draw(line);
-            getListenerPenCopy().draw(line);
+            getListenerPen_copy().draw(line);
         }
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
     public void mouseMoved(MouseEvent e) {
-
         //当多边形未完成时线条随鼠标移动
         if (n != 0) {
-            getListenerPen().drawImage(getDrawBoard().getDrawBoardCopy(), 0, 0, null);
+            getDrawBoard().refresh();
             Line2D line = new Line2D.Double(x[n - 1], y[n - 1], e.getX(), e.getY());
             getListenerPen().draw(line);
         }
