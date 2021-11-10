33ef3af204ee2fef6515f3bbbe970b03000b07a2
QianXmY <863119860@qq.com>
2021-11-10 23:40:46 +0800

    删去了一些不必要的重载函数名。对副本使用函数名做了对应的修改。修改了副本的使用语句。

--- PaintBoard/src/com/Listeners/LineListener.java
+++ PaintBoard/src/com/Listeners/LineListener.java
@@ -1,6 +1,5 @@
 package com.Listeners;
 
-import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Line2D;
 
@@ -16,6 +15,11 @@ public class LineListener extends DrawListener{
     }
 
     @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
     public void mousePressed(MouseEvent e) {
         startX = e.getX(); startY = e.getY();   //记录起点位置
     }
@@ -25,38 +29,20 @@ public class LineListener extends DrawListener{
         endX = e.getX(); endY = e.getY();   //记录终点位置
         Line2D line = new Line2D.Double(startX, startY, endX, endY);
         super.getListenerPen().draw(line);
-        super.getListenerPenCopy().draw(line);
+        super.getListenerPen_copy().draw(line);
     }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        getListenerPen().drawImage(getDrawBoard().getDrawBoardCopy(), 0 ,0, null);
+        getDrawBoard().refresh();
         endX = e.getX(); endY = e.getY();   //记录终点位置
         Line2D line = new Line2D.Double(startX, startY, endX, endY);
         getListenerPen().draw(line);
     }
 
     @Override
-    public void mouseMoved(MouseEvent e) {
+    public void actionPerformed(ActionEvent e) {
 
-    }
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
     }
 }
