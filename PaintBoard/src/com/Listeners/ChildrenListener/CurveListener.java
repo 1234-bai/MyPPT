c247c436ff0bea212d75d63b8cd50a7b98e283e2
Zihao <zihao-lee@outlook.com>
2021-11-14 22:36:22 +0800

    修复了圆形向左上拉拽问题、新增实心圆、实心矩形

--- PaintBoard/src/com/Listeners/ChildrenListener/CurveListener.java
+++ PaintBoard/src/com/Listeners/ChildrenListener/CurveListener.java
@@ -19,7 +19,8 @@ public class CurveListener extends DrawListener {
     public void mouseDragged(MouseEvent e) {
         nextX = e.getX(); nextY = e.getY(); //获得此时拖动过程中的鼠标坐标。
         Line2D line = new Line2D.Double(startX, startY, nextX, nextY);
-        getListenerPen().draw(line); getListenerPen_copy().draw(line);  //在画板上作画
+        getListenerPen().draw(line);
+        getListenerPen_copy().draw(line);  //在画板上作画
         startX = nextX; startY = nextY;
     }
 
