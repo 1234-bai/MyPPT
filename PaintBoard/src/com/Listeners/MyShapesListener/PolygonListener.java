2db696cae7d86c2c55eaf34d6ea6ac6fe1f4f93d
QianXmY <863119860@qq.com>
2021-11-15 21:32:13 +0800

    多边形监听器里直接使用向量，而不使用多边形。但在图形栈里使用多边形。

--- PaintBoard/src/com/Listeners/MyShapesListener/PolygonListener.java
+++ PaintBoard/src/com/Listeners/MyShapesListener/PolygonListener.java
@@ -1,52 +1,70 @@
 package com.Listeners.MyShapesListener;
 
 import com.Listeners.BaseListener.DrawListener;
+import com.MyShapes.ChildrenShapes.MyPolygon;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Line2D;
+import java.util.Vector;
 
 public class PolygonListener extends DrawListener {
 
     private static final int lastDict = 10; //判定构成多边形的距离参数
 
-    int[] x = new int[20];  //x坐标集合
-    int[] y = new int[20];  //y坐标集合
+    Vector<Integer> x = new Vector<>();  //x坐标集合
+    Vector<Integer> y = new Vector<>();  //y坐标集合
     int n = 0;    //点的个数
 
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
-        //记录点坐标
-        x[n] = e.getX();
-        y[n] = e.getY();
+        //记录点击的点坐标
+        int nowX = e.getX(), nowY = e.getY();
+        x.add(nowX); y.add(nowY);
         n++;
 
         //测试用，控制台显示鼠标点击的坐标
-        System.out.println(x[n - 1] + ", " + y[n - 1]);
+        System.out.println(nowX + ", " + nowY);
+
+        //当前点与第一个点的距离在lastDict以内，则形成收尾相连的多边形
+        int startX = x.get(0), startY = y.get(0);
+        if ((nowX - startX) * (nowX - startX) + (nowY - startY) * (nowY - startY) <= lastDict*lastDict  && n > 2) {
+
+            //最后一个点与第一个点坐标相同
+            x.set(n - 1, startX);
+            y.set(n - 1, startY);
+
+
+            getDrawBoard().refresh();  //最后一次选点多画的线删掉
+            //画出多边形最后一条线
+            Line2D line = new Line2D.Double(x.get(n - 2), y.get(n - 2), startX, startY);
+            getListenerPen().draw(line);
+            getListenerPen_copy().draw(line);
+
+            try{    //将画的的多边形送入图形栈
+                getContentsGroup().add(new MyPolygon(
+                        x,
+                        y,
+                        getListenerPen().getColor(),
+                        ((BasicStroke) getListenerPen().getStroke()).getLineWidth()
+                ));
+            } catch (IndexOutOfBoundsException ex){
+                System.out.println("多边形越界");
+                ex.printStackTrace();
+            }
+
+            n = 0;
+            x.clear(); y.clear();
+        }
 
         //将点连线
         if(n>1){
-            Line2D line = new Line2D.Double(x[n - 2],y[n - 2],x[n - 1],y[n - 1]);
+            Line2D line = new Line2D.Double(x.get(n - 2), y.get(n - 2), nowX, nowY);
             getListenerPen().draw(line);
             getListenerPen_copy().draw(line);
         }
 
-        //当前点与第一个点的距离在lastDict以内，则形成收尾相连的多边形
-        if ((x[n - 1] - x[0]) * (x[n - 1] - x[0]) + (y[n - 1] - y[0]) * (y[n - 1] - y[0]) <= lastDict*lastDict  && n > 2) {
-
-            //最后一个点与第一个点坐标相同
-            x[n - 1] = x[0];
-            y[n - 1] = y[0];
-
-            //画出多边形
-            Polygon polygon = new Polygon(x, y, n);
-            getDrawBoard().refresh();  //最后一次选点多画的线删掉
-            super.getListenerPen().draw(polygon);
-            super.getListenerPen_copy().draw(polygon);
-            n = 0;
-        }
     }
 
     @Override
@@ -55,7 +73,7 @@ public class PolygonListener extends DrawListener {
         //当多边形未完成时线条随鼠标移动
         if (n != 0) {
             getDrawBoard().refresh();
-            Line2D line = new Line2D.Double(x[n - 1], y[n - 1], e.getX(), e.getY());
+            Line2D line = new Line2D.Double(x.get(n - 1), y.get(n - 1), e.getX(), e.getY());
             getListenerPen().draw(line);
         }
     }
