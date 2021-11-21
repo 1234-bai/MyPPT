42f8a375f37d7187519d447a6859f69a78e2305b
RoboAC <Cedric_hxc2000@outlook.com>
2021-11-21 21:14:36 +0800

    增加适用于图形移动撤销重做的方法

--- PaintBoard/src/com/Operations/BaseOperation/BaseOps.java
+++ PaintBoard/src/com/Operations/BaseOperation/BaseOps.java
@@ -2,26 +2,43 @@ package com.Operations.BaseOperation;
 
 import com.Paint.DrawJPanel;
 
-public abstract class BaseOps {
+public class BaseOps {
 
     /**
      * 执行操作时将操作行为压栈
      *
      * @param drawBoard 操作所在的画板
      */
-    public abstract void addOperation(DrawJPanel drawBoard);
+    public void addOperation(DrawJPanel drawBoard) {
+
+    }
+
+    /**
+     * 执行操作时将操作行为压栈
+     *
+     * @param drawBoard 操作所在的画板
+     * @param translateX 偏移量X
+     * @param translateY 偏移量Y
+     */
+    public void addOperation(DrawJPanel drawBoard, double translateX, double translateY) {
+
+    }
 
     /**
      * 执行撤销操作时的处理
      *
      * @param drawBoard 操作所在的画板
      */
-    public abstract void revoke(DrawJPanel drawBoard);
+    public void revoke(DrawJPanel drawBoard) {
+
+    }
 
     /**
      * 执行重做操作时的处理
      *
      * @param drawBoard 操作所在的画板
      */
-    public abstract void redo(DrawJPanel drawBoard);
+    public void redo(DrawJPanel drawBoard) {
+
+    }
 }
